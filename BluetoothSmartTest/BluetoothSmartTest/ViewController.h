7de500a57d04be99875fec20a93ff3c249069b9c
FF <haruhito.fuji@gmail.com>
2015-03-28 20:26:45 +0900

    BluetoothSmartコントロール実装
    Olympus Air A01のBluetooth Smartをコントロールする
    ・BluetoothSmartの待ち処理が一定時間固定処理
    ・ボタンないが非同期化されていないので処理が全部終わるまで凹んだまま

--- BluetoothSmartTest/BluetoothSmartTest/ViewController.h
+++ BluetoothSmartTest/BluetoothSmartTest/ViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#import <CoreBluetooth/CoreBluetooth.h>
 
 @interface ViewController : UIViewController
 
--- BluetoothSmartTest/BluetoothSmartTest/ViewController.m
+++ BluetoothSmartTest/BluetoothSmartTest/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+<CBCentralManagerDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *infoLabel;
+//CoreBluetooth
+@property (strong,nonatomic) CBCentralManager *centralManager;
 @end
 
 @implementation ViewController
@@ -23,6 +26,11 @@
     AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
     [delegate addObserver:self forKeyPath:@"BletoothSmartName" options:NSKeyValueObservingOptionNew context:nil];
     [delegate addObserver:self forKeyPath:@"BletoothSmartPasscode" options:NSKeyValueObservingOptionNew context:nil];
+    
+    //CoreBluetooth Initialize
+    NSDictionary *option = @{ CBCentralManagerOptionShowPowerAlertKey: @YES};
+    self.centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0) options:option];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -35,8 +43,35 @@
     [OACentralConfiguration requestConfigurationURL:@"BluetoothSmartTest.OlympusCameraKit.FromF.github.com"];
 }
 - (IBAction)PowerOnTestAction:(id)sender {
+    BOOL result = YES;
+    if (result) {
+        result = [self scanBluetoothSmart];
+    }
+    if (result) {
+        OLYCamera *camera = AppDelegateCamera();
+        result = [camera wakeup:nil];
+    }
+    NSLog(@"result(%d)",result);
 }
 - (IBAction)TakePictureAction:(id)sender {
+    BOOL result = YES;
+    OLYCamera *camera = AppDelegateCamera();
+    if (result) {
+        result = [self scanBluetoothSmart];
+    }
+    if (result) {
+        result = [camera connect:OLYCameraConnectionTypeBluetoothLE error:nil];
+    }
+    if ((result) && (camera.connected)) {
+        result = [camera changeRunMode:OLYCameraRunModeRecording error:nil];
+    }
+    if ((result) && (camera.connected)) {
+        [camera takePicture:nil progressHandler:nil completionHandler:nil errorHandler:nil];
+    }
+    if ((result) && (camera.connected)) {
+        [camera disconnectWithPowerOff:YES error:nil];
+    }
+    NSLog(@"result(%d)",result);
 }
 
 #pragma mark - KVO
@@ -48,4 +83,79 @@
     }
 }
 
+#pragma mark - BluetoothSmart Scan
+-(BOOL)scanBluetoothSmart
+{
+    BOOL result = YES;
+    
+    if (result) {
+        if (self.centralManager.state != CBCentralManagerStatePoweredOn) {
+            NSLog(@"BluetoothSmart device is not powered on!");
+            result = NO;
+        }
+    }
+    
+    if (result) {
+        dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            OLYCamera *camera = AppDelegateCamera();
+            camera.bluetoothPeripheral = nil;
+            
+            [self.centralManager stopScan];
+            NSDictionary *option = @{ CBCentralManagerScanOptionAllowDuplicatesKey: @NO };
+            [self.centralManager scanForPeripheralsWithServices:[OLYCamera bluetoothServices] options:option];
+            //10sec wait
+            [NSThread sleepForTimeInterval:10.0f];
+            if (camera.bluetoothPeripheral) {
+                [self.centralManager connectPeripheral:camera.bluetoothPeripheral options:nil];
+                //10sec wait
+                [NSThread sleepForTimeInterval:2.0f];
+                //
+                if (camera.bluetoothPeripheral.state != CBPeripheralStateConnected) {
+                    NSLog(@"BluetoothSmart device is not conneted.");
+                    camera.bluetoothPeripheral = nil;
+                }
+            } else {
+                NSLog(@"BluetoothSmart device is not found.");
+            }
+            
+        });
+        //Check Result
+        OLYCamera *camera = AppDelegateCamera();
+        if (!camera.bluetoothPeripheral) {
+            result = NO;
+        }
+    }
+    if (result) {
+        AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+        OLYCamera *camera = AppDelegateCamera();
+        camera.bluetoothPassword = delegate.BletoothSmartPasscode;
+    }
+    
+    return result;
+}
+#pragma mark - CBCentralManagerDelegate
+-(void)centralManagerDidUpdateState:(CBCentralManager *)central
+{
+    //None
+}
+
+-(void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI
+{
+    AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+    OLYCamera *camera = AppDelegateCamera();
+    
+    if (delegate.BletoothSmartName && delegate.BletoothSmartName.length > 0) {
+        if ([[advertisementData objectForKey:CBAdvertisementDataLocalNameKey]isEqualToString:delegate.BletoothSmartName]) {
+            //Store peripheral infomation
+            camera.bluetoothPeripheral = peripheral;
+            //peripheral scan stop
+            [self.centralManager stopScan];
+        }
+    }
+}
+
+-(void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral
+{
+    NSLog(@"BluetoothSmart: connected to peripheral(%@)",peripheral.name);
+}
 @end
