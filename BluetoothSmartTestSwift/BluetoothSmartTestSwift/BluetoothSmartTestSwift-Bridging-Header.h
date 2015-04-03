1783da5a84faa7b5177dacb257744a6c5ed61727
FF <haruhito@j4700112.olympus.co.jp>
2015-04-03 10:01:54 +0900

    CameraKit import

---
+++ BluetoothSmartTestSwift/BluetoothSmartTestSwift/BluetoothSmartTestSwift-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  BluetoothSmartTestSwift-Bridging-Header.h
+//  BluetoothSmartTestSwift
+//
+//  Created by haruhito on 2015/04/03.
+//  Copyright (c) 2015年 FromF. All rights reserved.
+//
+
+#ifndef BluetoothSmartTestSwift_BluetoothSmartTestSwift_Bridging_Header_h
+#define BluetoothSmartTestSwift_BluetoothSmartTestSwift_Bridging_Header_h
+
+#import <OLYCameraKit/OLYCamera.h>
+
+#endif
