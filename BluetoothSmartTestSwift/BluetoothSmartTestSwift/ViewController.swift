fce6c298ca45a12a85cef59acbf757a34b108912
FF <haruhito.fuji@gmail.com>
2015-04-04 16:35:52 +0900

    OA.CentralからBletoothSmartペアリング情報取得実装完了
    Olympus Camera Kitを使ってBluetooth Smartを操作してみる
    第１弾 OA.Centralからペアリング情報を受け取る
    
    Keyword:Olympus Camera Kit , OA.Central , Bluetooth Smart , iOS ,
    Olympus Air A01 , OPC , swift

--- BluetoothSmartTestSwift/BluetoothSmartTestSwift/ViewController.swift
+++ BluetoothSmartTestSwift/BluetoothSmartTestSwift/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , CBCentralManagerDelegate {
 
     @IBOutlet weak var infoLabel: UILabel!
-    
+    //CoreBluetooth
+    var centralManager: CBCentralManager!
+
     //AppDelegate instance
     var appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as AppDelegate
     
@@ -22,6 +25,10 @@ class ViewController: UIViewController {
         
         //Notification Regist
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "bluetoothSmartUpdated:", name: appDelegate.BluetoothSmartNotification, object: nil)
+        
+        //CoreBluetooth Initialize
+        var option : NSDictionary = [CBCentralManagerOptionShowPowerAlertKey : true]
+        self.centralManager = CBCentralManager(delegate: self, queue: dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0) , options: option)
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,9 +41,116 @@ class ViewController: UIViewController {
         OACentralConfiguration.requestConfigurationURL("BluetoothSmartTestSwift.OlympusCameraKit.FromF.github.com")
     }
 
+    @IBAction func PowerOnTestAction(sender: AnyObject) {
+        var camera = AppDelegate.sharedCamera
+        var result : Bool = true
+        
+        if (result) {
+            result = self.scanBluetoothSmart()
+        }
+        
+        if (result) {
+            result = camera.wakeup(nil);
+        }
+        println("result \(result)")
+    }
+    
+    @IBAction func TakePictureAction(sender: AnyObject) {
+        var camera = AppDelegate.sharedCamera
+        var result : Bool = true
+        
+        if (result) {
+            result = self.scanBluetoothSmart()
+        }
+        
+        if (result) {
+            result = camera.connect(OLYCameraConnectionTypeBluetoothLE, error: nil)
+        }
+        if ((result) & (camera.connected)) {
+            result = camera.changeRunMode(OLYCameraRunModeRecording, error: nil)
+        }
+        if ((result) & (camera.connected)) {
+            camera.takePicture(nil, progressHandler: nil, completionHandler: nil, errorHandler: nil)
+        }
+        if ((result) & (camera.connected)) {
+            camera.disconnectWithPowerOff(true, error: nil)
+        }
+        println("result \(result)")
+    }
+    
 // MARK: - Notification
     func bluetoothSmartUpdated(notification : NSNotification?) {
         self.infoLabel.text = "Name[\(appDelegate.BluetoothSmartName)] Passcode[\(appDelegate.BluetoothSmartPasscode)]"
+    }
+    
+// MARK: - BluetoothSmart Scan
+    func scanBluetoothSmart() ->Bool {
+        var result : Bool = true
+        
+        if (self.centralManager.state != CBCentralManagerState.PoweredOn) {
+            println("BluetoothSmart device is not powered on!")
+            result = false
+        }
+        
+        if (result) {
+            dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+                var camera = AppDelegate.sharedCamera
+                camera.bluetoothPeripheral = nil
+                
+                self.centralManager.stopScan()
+                //NSDictionary *option = @{ CBCentralManagerScanOptionAllowDuplicatesKey: @NO };
+                var option : NSDictionary = [CBCentralManagerScanOptionAllowDuplicatesKey : false]
+                self.centralManager.scanForPeripheralsWithServices(OLYCamera.bluetoothServices(), options: nil)
+                //10sec wait
+                NSThread.sleepForTimeInterval(10.0)
+                if (camera.bluetoothPeripheral != nil) {
+                    self.centralManager.connectPeripheral(camera.bluetoothPeripheral, options: nil)
+                    //2sec wait
+                    NSThread.sleepForTimeInterval(2.0)
+                    if (camera.bluetoothPeripheral.state != CBPeripheralState.Connected) {
+                        println("BluetoothSmart device is not conneted.")
+                        camera.bluetoothPeripheral = nil
+                    }
+                } else {
+                    println("BluetoothSmart device is not found.")
+                }
+            })
+            //Check Result
+            var camera = AppDelegate.sharedCamera
+            if (camera.bluetoothPeripheral == nil) {
+                result = false
+            }
+        }
+        
+        if (result) {
+            var camera = AppDelegate.sharedCamera
+            camera.bluetoothPassword = appDelegate.BluetoothSmartPasscode
+        }
+        
+        return result;
+    }
+    
+// MARK: - CBCentralManagerDelegate
+    func centralManagerDidUpdateState(central: CBCentralManager!) {
+        //NONE
+    }
+
+    func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: [NSObject : AnyObject]!, RSSI: NSNumber!) {
+        println("peripheral: \(peripheral)")
+        
+        var camera = AppDelegate.sharedCamera
+        if (appDelegate.BluetoothSmartName.length > 0) {
+            if (advertisementData[CBAdvertisementDataLocalNameKey] as NSString == appDelegate.BluetoothSmartName) {
+                //Store peripheral infomation
+                camera.bluetoothPeripheral = peripheral;
+                //peripheral scan stop
+                centralManager.stopScan()
+            }
+        }
+    }
+    
+    func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
+        println("BluetoothSmart: connected to peripheral(\(peripheral.name))")
     }
 }
 
